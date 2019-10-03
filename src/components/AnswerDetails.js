635ceec7a402047401929812939e14775b00c7be
Mao Li <anrenyil@gmail.com>
2019-10-03 14:14:24 -0700

    class components

--- src/components/AnswerDetails.js
+++ src/components/AnswerDetails.js
@@ -4,13 +4,19 @@ import '../styles/AnswerDetails.css';
 
 // AnswerDetails Component
 function AnswerDetails(props) {
+    // debugger;
     return (
         <div className="ui segment AnswerDetails">
             <div className="ui header">Answer Details</div>
             <p>
                 {props.body} <br />
-                <ShowAttribution author={props.author} created_at={props.created_at} />
+                <ShowAttribution author={props.author_fullname} created_at={props.created_at} />
             </p>
+            <button className="ui right floated red button" onClick={() => {
+                props.onDeleteClick()
+            }}>
+                Delete
+			</button>
         </div>
     );
 }
--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionDetails from './QuestionDetails';
-import AnswerDetails from './AnswerDetails';
-import questionData from './questionData';
-import AnswerList from './AnswerList'
+import AnswerList from './AnswerList';
+import oneQuestionData from './oneQuestionData';
+import DeleteButton from './DeleteButton';
 
 // Question Show Component
-function QuestionShowPage(props) {
-    return (
-        <div>
-            <h1>Question Number {props.number}</h1>
-            <QuestionDetails
-                title={questionData[0].title}
-                body={questionData[0].body}
-                view_count={questionData[0].view_count}
-                author={questionData[0].author.full_name}
-                created_at={new Date(questionData[0].created_at)}
-            />
+class QuestionShowPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: { ...oneQuestionData }
+        };
+        this.deleteAnswer = this.deleteAnswer.bind(this);
+    }
+    deleteQuestion() {
+        this.setState({
+            question: null
+        });
+    }
+    deleteAnswer(answerId) {
+        this.setState((state) => {
+            return {
+                question: {
+                    ...state.question,
+                    answers: state.question.answers.filter((answer) => answer.id !== answerId)
+                }
+            };
+        });
+    }
+    render() {
+        if (!this.state.question) {
+            return (
+                <main>
+                    <h1>Question Does not exist</h1>
+                </main>
+            );
+        }
+        return (
+            <div>
+                <h1>Question Number {this.props.number}</h1>
+                <QuestionDetails
+                    // title={this.state.question.title}
+                    // body={this.state.question.body}
+                    // view_count={this.state.question.view_count}
+                    // author={this.state.question.full_name}
+                    // created_at={this.state.question.created_at}
+                    {...this.state.question}
+                />
+                <DeleteButton onDeleteClick={() => this.deleteQuestion()} />
+                <AnswerList onAnswerDelete={this.deleteAnswer} answers={this.state.question.answers} />
 
-            <AnswerList answers={questionData[0].answers} />
-
-            {/* <AnswerDetails body="Red." author={{ full_name: "Michael Owen" }} created_at={new Date()} /> */}
-        </div>
-    );
+                {/* <AnswerDetails body="Red." author={{ full_name: "Michael Owen" }} created_at={new Date()} /> */}
+            </div>
+        );
+    }
 }
+
 export default QuestionShowPage;
